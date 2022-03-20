37d548cfb21c3cda077884cdf51629fb0aabc5e4
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-03-20 07:55:37 -0400

    added timeblocks and  conditional to update css color with time

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,19 +16,30 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 var dayHours = function(){
 
-    // loop to create all timeblocks 
-    
-        for(var i = 8; i < 17; i++){
-            var workDay = workHour.add(1, "hour").format("h A");
-    
-            var row = $("<div></div>").addClass("row time-block");
-            var hour = $("<div></div>").addClass("col-1 hour");
-            var textInputEl = $("<textarea></textarea>").addClass("col-10 textInputClass");
-            var saveBtn = $("<button></button>").addClass("col-1 fa fa-save saveBtn");
-    
-            $(".container").append(row);
-            $(row).append(hour,textInputEl,saveBtn);
+// loop to create all timeblocks 
 
+    for(var i = 8; i < 17; i++){
+        var workDay = workHour.add(1, "hour").format("h A");
+
+        var row = $("<div></div>").addClass("row time-block");
+        var hour = $("<div></div>").addClass("col-1 hour");
+        var textInputEl = $("<textarea></textarea>").addClass("col-10 textInputClass");
+        var saveBtn = $("<button></button>").addClass("col-1 fa fa-save saveBtn");
+
+        $(".container").append(row);
+        $(row).append(hour,textInputEl,saveBtn);
+
+        $(hour).text(workDay);
+
+//  change css class when conditions are met
+        if(i < currentHour){
+            $(textInputEl).attr("class", "col-10 row past")
+        }else if(i > currentHour){
+            $(textInputEl).attr("class", "col-10 row future")
+        }else{
+            $(textInputEl).attr("class", "col-10 row present")
         }
+
+    }
 };
 dayHours();
