fc35820e75c0370c9bb2002f998151b2e263ca86
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-03-20 07:48:19 -0400

    added dayHours() to create and append elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,24 @@ var saveBtn;
 var currentHour = moment().format("HH");
 var workHour = moment().startOf('day').add(7, "hour");
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+// function with for loop to create timeblocks with textarea and save button
+
+var dayHours = function(){
+
+    // loop to create all timeblocks 
+    
+        for(var i = 8; i < 17; i++){
+            var workDay = workHour.add(1, "hour").format("h A");
+    
+            var row = $("<div></div>").addClass("row time-block");
+            var hour = $("<div></div>").addClass("col-1 hour");
+            var textInputEl = $("<textarea></textarea>").addClass("col-10 textInputClass");
+            var saveBtn = $("<button></button>").addClass("col-1 fa fa-save saveBtn");
+    
+            $(".container").append(row);
+            $(row).append(hour,textInputEl,saveBtn);
+
+        }
+};
+dayHours();
